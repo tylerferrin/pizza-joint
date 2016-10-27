e787e0e1e9b2af7682e38ecb5d755f5348653c5e
Tyler Ferrin <tyferrin@gmail.com>
2016-10-27 13:53:04 -0700

    add basic styling and menu to the page

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+// BUSINESS _________________________________________
+
+function Order() {
+  this.pizza = [];
+  this.orderTotal = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FRONT-END ---------------------------------------
+$(document).ready(function() {
+
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+SPEC 1
+Input: click order online  
+output: a new order object is created
