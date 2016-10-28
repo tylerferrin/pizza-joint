550533a43dbf3a9ee7faf423f38027bbdae42e61
Tyler Ferrin <tyferrin@gmail.com>
2016-10-28 16:54:14 -0700

    create the menu, create pizza and order objects when add to order is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@
 // BUSINESS _________________________________________
 
 function Order() {
-  this.pizza = [];
+  this.orderItems = [];
   this.orderTotal = 0;
+  this.orderName;
+  this.orderAddress;
+}
+
+function Pizza(size, crust) {
+  this.pizzaToppings = [];
+  this.pizzaSize = size;
+  this.pizzaCrust = crust;
 }
 
 
@@ -16,20 +24,40 @@ function Order() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // FRONT-END ---------------------------------------
-$(document).ready(function() {
 
+var currentOrder = new Order();
+
+$("document").ready(function() {
+    $("a#order-start").click(function(event) {
+      event.preventDefault();
+
+      $(".header").slideToggle();
+      $("#landing-div").slideToggle();
+      $("#order-div").slideToggle(function() {
+        $("#order-row").fadeIn(250);
+      });
+
+      $("#addpizza").click(function () {
+        $(".addtoppings").show();
+      });
+
+      $("button#add2order").click(function() {
+        var pizzasize = $("select#pizzasize").val();
+        var pizzacrust = $("select#pizzacrust").val();
+        var newpizza = new Pizza(pizzasize, pizzacrust);
+
+        $("input:checkbox[name=pizza-toppings]:checked").each(function() {
+          var newtoppings = $(this).val();
+          newpizza.pizzaToppings.push(newtoppings);
+        });
+
+        currentOrder.orderItems.push(newpizza);
+        console.log(currentOrder);
+      });
+    })
 });
+
+function animateStuff(){
+  $('body').hide();
+}
--- readme.md
+++ readme.md
@@ -4,6 +4,19 @@
 
 
 
-SPEC 1
-Input: click order online  
-output: a new order object is created
+SPEC
+Input: click on "order online"
+output: order div slides up / hide landing-div & header img.
+
+SPEC
+Input: click add pizza
+output:toppings menu shows
+
+
+SPEC
+Input: click add to order
+output: new pizza object inside current order
+
+SPEC
+Input: when add to order button is pressed
+Output: current order div is updated and order is displayed
